b9b882d615ebeb0803e93b8b4b1c642d9d7f853f
Khoa Thai <khoatda20411c@st.uel.edu.vn>
2022-11-28 19:46:59 +0700

    cap nhat mot vai man hinh moi len

---
+++ app/src/main/java/com/nhom3/diduclub_app/AccountSettingFragment.java
@@ -0,0 +1,14 @@
+package com.nhom3.diduclub_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountSettingFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_account_setting);
+    }
+}
---
+++ app/src/main/java/com/nhom3/diduclub_app/CartNotLoginFragment.java
@@ -0,0 +1,14 @@
+package com.nhom3.diduclub_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CartNotLoginFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_cart_not_login);
+    }
+}
