8df328c6cd64ead3b67f74e944feb5f7fdf5ee8a
Nguyen-Ho-Trung-Tin <tinnht20411c@st.uel.edu.vn>
2022-11-28 23:03:40 +0700

    capnhat mainactivity

--- app/src/main/java/com/nhom3/diduclub_app/MainActivity.java
+++ app/src/main/java/com/nhom3/diduclub_app/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tai_khoan);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
     }
 }
