d63a423f1e77ab8ed76445ab6525d05c87f722c0
Nguyen-Ho-Trung-Tin <tinnht20411c@st.uel.edu.vn>
2022-11-29 03:45:49 +0700

    capnhatandactivity,fragment.xml

---
+++ app/src/main/java/com/nhom3/diduclub_app/fragment_main.java
@@ -0,0 +1,18 @@
+package com.nhom3.diduclub_app;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+public class fragment_main extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main,container,false);
+    }
+}
