b9718fc74b5bd8c1d403e5c4da030dbc6af649ce
Nguyen-Ho-Trung-Tin <tinnht20411c@st.uel.edu.vn>
2022-11-29 21:17:22 +0700

    update lich su don hang

--- app/src/main/java/com/nhom3/diduclub_app/MainActivity.java
+++ app/src/main/java/com/nhom3/diduclub_app/MainActivity.java
@@ -1,15 +1,16 @@
 package com.nhom3.diduclub_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_otpactivity);
+        setContentView(R.layout.activity_main);
+        fragment_main fragmentmain=(com.nhom3.diduclub_app.fragment_main) getFragmentManager().findFragmentById(R.id.fragment_main);
 
     }
 }
