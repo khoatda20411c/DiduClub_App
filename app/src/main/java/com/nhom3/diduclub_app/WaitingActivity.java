501771bbab6098dc9f6e1e16f59918d5bef4f177
Khoa Thai <khoatda20411c@st.uel.edu.vn>
2022-12-04 00:14:56 +0700

    thay doi manifest va chuyen Waiting activity len dau tien

--- app/src/main/java/com/nhom3/diduclub_app/WaitingActivity.java
+++ app/src/main/java/com/nhom3/diduclub_app/WaitingActivity.java
@@ -2,13 +2,18 @@ package com.nhom3.diduclub_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class WaitingActivity extends AppCompatActivity {
+    ImageView imvNext_activity_waiting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiting);
+        imvNext_activity_waiting = findViewById(R.id.imv_Next_activity_waiting);
+
     }
 }
