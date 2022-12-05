123b58231f713f8a788513fd99e466559d3bf939
Nguyen-Ho-Trung-Tin <tinnht20411c@st.uel.edu.vn>
2022-12-05 22:37:00 +0700

    capnhat

--- app/src/main/java/com/nhom3/diduclub_app/LoadingActivity.java
+++ app/src/main/java/com/nhom3/diduclub_app/LoadingActivity.java
@@ -2,13 +2,25 @@ package com.nhom3.diduclub_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoadingActivity extends AppCompatActivity {
+    Button btngetstarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        btngetstarted= findViewById(R.id.btngetstarted);
+        btngetstarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(LoadingActivity.this,WaitingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/nhom3/diduclub_app/WaitingActivity.java
+++ app/src/main/java/com/nhom3/diduclub_app/WaitingActivity.java
@@ -1,28 +1,41 @@
 package com.nhom3.diduclub_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class WaitingActivity extends AppCompatActivity {
+    FragmentManager manager;
     ImageView imvNext_activity_waiting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiting);
-
-        imvNext_activity_waiting = findViewById(R.id.imv_Next_activity_waiting);
-
-        imvNext_activity_waiting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(WaitingActivity.this, Waiting1Fragment.class);
-                startActivity(intent);
-            }
-        });
+                manager=getSupportFragmentManager();
+                FragmentTransaction transaction=manager.beginTransaction();
+                transaction.replace(R.id.layoutwaiting_container, new fragment_waiting0());
+                transaction.commit();
+//        fragment_waiting0 fragment_waiting0ra =(fragment_waiting0) getFragmentManager().findFragmentById(R.id.frag);
+//
+//        imvNext_activity_waiting = findViewById(R.id.imv_Next_activity_waiting);
+//
+//        imvNext_activity_waiting.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                manager=getSupportFragmentManager();
+//                FragmentTransaction transaction=manager.beginTransaction();
+//                transaction.replace(R.id.layoutwaiting_container, new fragment_waiting0());
+//                transaction.commit();
+//
+//
+//            }
+//        });
     }
 }
