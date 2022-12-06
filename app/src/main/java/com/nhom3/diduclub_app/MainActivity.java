9854b9623880589c3d7b558cf1e5d527108dffab
Nguyen-Ho-Trung-Tin <tinnht20411c@st.uel.edu.vn>
2022-12-07 01:49:41 +0700

    capnhat

--- app/src/main/java/com/nhom3/diduclub_app/MainActivity.java
+++ app/src/main/java/com/nhom3/diduclub_app/MainActivity.java
@@ -9,6 +9,9 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -21,6 +24,8 @@ import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
+    ViewPager mViewpager;
+    FragmentManager manager;
 
 
     @Override
@@ -28,30 +33,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Linkview();
-        fragment_main fragmentmain = (fragment_main) getFragmentManager().findFragmentById(R.id.fragment_main);
-        fragmentmain.btn_login_fragmentmain.setOnClickListener(new View.OnClickListener() {
+        setupViewpager();
+ //       fragment_main fragmentmain = (fragment_main) getFragmentManager().findFragmentById(R.id.fragment_main_container);
+ //       fragmentmain.btn_login_fragmentmain.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent=new Intent(MainActivity.this,LoginmethodActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,LoginmethodActivity.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.cart:
+                        Toast.makeText(MainActivity.this, "Cart", Toast.LENGTH_SHORT).show();
 
+                        break;
+
+                    case R.id.sale:
+                        Toast.makeText(MainActivity.this, "Sale", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.notifications:
+                        Toast.makeText(MainActivity.this, "Notification", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.account:
+                        Toast.makeText(MainActivity.this, "Account", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
             }
         });
 
+    }
 
-
-
-
-
-
-
+    private void setupViewpager() {
 
     }
 
     private void Linkview() {
-        bottomNavigationView = findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_nar);
+        mViewpager= findViewById(R.id.viewpager);
 
     }
+
+
 
 
 }
